a33f9f749ed3f3e533b670520720004471fd9513
Wael Ben Zid El Guebsi <wbenzid@meta.com>
2025-07-23 10:57:13 +0100

    Person model added

---
+++ src/person.py
@@ -0,0 +1,76 @@
+"""
+This module provides a comprehensive way to represent family relationships
+that makes Islamic inheritance calculations easier and more accurate.
+"""
+
+from collections import defaultdict, deque
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Dict, List, Optional, Set
+
+
+class Religion(Enum):
+    ISLAM = "Islam"
+    CHRISTIANITY = "christianity"
+    JEWISH = "jewish"
+    OTHER = "other"
+
+
+class Gender(Enum):
+    UNKNOWN = "unknown"
+    MALE = "male"
+    FEMALE = "female"
+
+
+@dataclass
+class Person:
+    """Represents a person in the family tree."""
+
+    name: str
+    gender: Gender
+    religion: Religion = Religion.ISLAM
+    birth_year: Optional[int] = None
+    death_year: Optional[int] = None
+
+    # Relationships
+    father: Optional["Person"] = None
+    mother: Optional["Person"] = None
+    spouses: Set["Person"] = field(default_factory=set)
+    children: Set["Person"] = field(default_factory=set)
+
+    def __post_init__(self):
+        """Validate person data."""
+        if self.death_year and self.birth_year and self.death_year < self.birth_year:
+            raise ValueError("Death year cannot be before birth year")
+
+    def add_father(self, father: "Person"):
+        if self.father and self.father != father:
+            raise ValueError("Cannot replace father!")
+        self.father = father
+        father.children.add(self)
+
+    def add_mother(self, mother: "Person"):
+        if self.mother and self.mother != mother:
+            raise ValueError("Cannot replace mother!")
+        self.mother = mother
+        mother.children.add(self)
+
+    def add_spouse(self, spouse: "Person"):
+        # TODO: for now, let's ignore unknow gender
+        if self.gender == spouse.gender:
+            raise ValueError("Invalid gender")
+        self.spouses.add(spouse)
+
+        if self.gender == Gender.FEMALE and len(self.spouses) > 1:
+            raise ValueError("Too many husbands")
+
+    def add_child(self, child: "Person"):
+        match self.gender:
+            case Gender.MALE:
+                child.add_father(self)
+            case Gender.FEMALE:
+                child.add_mother(self)
+            case _:
+                raise ValueError(
+                    "Cannot determine relationship. Please add it using add_father() or add_mother() APIs!"
+                )
